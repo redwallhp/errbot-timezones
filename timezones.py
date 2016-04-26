82ab3a6146ff451e6f77cc23fa4904d5b78021d7
redwallhp <mcredwallhp@gmail.com>
2016-04-26 00:12:06 -0400

    Initial commit

---
+++ timezones.py
@@ -0,0 +1,83 @@
+from errbot import BotPlugin, botcmd
+import re, pytz, datetime
+import tz_definitions
+
+class Timezones(BotPlugin):
+
+
+    def activate(self):
+        super().activate()
+        self.user_zones = self.get("USER_ZONES", {})
+        self['USER_ZONES'] = self.user_zones
+
+
+    @botcmd
+    def settz(self, msg, args):
+        you = msg.frm.person.replace('@', '')
+        if len(args) > 0:
+            if args in pytz.common_timezones:
+                self.user_zones[you] = args
+                self['USER_ZONES'] = self.user_zones
+                return "Updated your timezone"
+            else:
+                return "You must enter a valid timezone. e.g. America/New_York"
+        else:
+            return "Usage: settz America/New_York"
+
+
+    @botcmd
+    def tz(self, msg, args):
+        """ Convert a timezone """
+        if len(args) < 1:
+            return "Usage: tz <time> <input timezone> [output timezone]"
+
+        hour, minute, second, remainder = self.parse_time(args)
+        zargs = remainder.split(' ')
+        input_zone = self.parse_timezone(zargs[0])
+        if len(zargs) > 1:
+            output_zone = self.parse_timezone(zargs[1])
+        else:
+            output_zone = self.user_zones.get(msg.frm.person.replace('@', ''), 'UTC')
+        original_time = "%s:%s" % (hour, minute)
+
+        dt = datetime.datetime.today()
+        dt = dt.replace(hour = hour, minute = minute, second = second)
+        idt = pytz.timezone(input_zone).localize(dt)
+        new_dt = idt.astimezone(pytz.timezone(output_zone))
+        original_time = dt.strftime("%I:%M %p")
+        new_time = new_dt.strftime("%I:%M %p")
+
+        yield "**%s** %s *(converted from %s %s)*" % (new_time, output_zone, original_time, input_zone)
+
+
+    def parse_time(self, text):
+        regex = re.compile(r'([0-9]?[0-9]):([0-5][0-9])(:[0-5][0-9])?( PM| AM)?', re.I)
+        hour = 0
+        minute = 0
+        second = 0
+        match = regex.search(text)
+        if match:
+            hour = int(match.group(1))
+            # convert to 24-hour format
+            if str(match.group(4)).lower() == " pm":
+                if hour != 12:
+                    hour = hour + 12
+            if str(match.group(4)).lower() == " am":
+                if hour == 12:
+                    hour = 0
+            minute = int(match.group(2))
+            if match.group(3):
+                second = int(match.group(3)[1:])
+        remainder = regex.sub('', text).strip()
+        return hour, minute, second, remainder
+
+
+    def parse_timezone(self, text):
+        if text.lower() in tz_definitions.ABBREVIATIONS:
+            return tz_definitions.ABBREVIATIONS.get(text.lower(), "UTC")
+        elif text in pytz.common_timezones:
+            return text
+        elif text in self.user_zones:
+            return self.user_zones[text]
+        else:
+            return "UTC"
---
+++ tz_definitions.py
@@ -0,0 +1,30 @@
+ABBREVIATIONS = {
+        "utc" : "UTC",
+        "gmt" : "UTC",
+        "bst" : "Europe/London",
+        "wet" : "Europe/London",
+        "est" : "America/New_York",
+        "edt" : "America/New_York",
+        "eastern" : "America/New_York",
+        "pst" : "America/Los_Angeles",
+        "pdt" : "America/Los_Angeles",
+        "pacific" : "America/Los_Angeles",
+        "mst" : "America/Phoenix",
+        "mdt" : "America/Denver",
+        "mountain" : "America/Denver",
+        "cst" : "America/Chicago",
+        "cdt" : "America/Chicago",
+        "central" : "America/Chicago",
+        "aest" : "Australia/Brisbane",
+        "aedt" : "Australia/Sydney",
+}
+
+
+OUTPUT = [
+        "UTC",
+        "America/New_York",
+        "America/Los_Angeles",
+        "America/Denver",
+        "America/Chicago",
+        "Australia/Brisbane"
+]
